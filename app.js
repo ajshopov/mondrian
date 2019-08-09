148ba31207977e2e9f4bcac40c5ef9a2623631c3
ajshopov <alexanderjshopov@gmail.com>
2019-08-09 14:56:02 +1000

    added random splitting and colours

--- app.js
+++ app.js
@@ -1,55 +1,69 @@
-console.log('ready')
-
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-canvas.height = 600;
-canvas.width = 600;
+const size = 600;
+canvas.height = size;
+canvas.width = size;
+// var dpr = window.devicePixelRatio;
+// console.log(dpr)
 ctx.lineWidth = 8;
+const step = size / 13;
+// let countSplit = 0;
+// let countSX = 0;
+// let countSY = 0;
+const white = '#F2F5F1';
+const colors = ['#D40920', '#1356A2', '#F7D842'];
+
 
 let squares = [{
   x: 0,
   y: 0,
-  width: 600,
-  height: 600
+  width: size,
+  height: size
 }];
-console.table(squares)
+// console.table(squares)
 
 
 function splitSquaresAt(coordinates) {
   const { x, y } = coordinates;
   console.log(x, y);
+  // countSplit+=1;
 
   for (var i = squares.length - 1; i >= 0; i--) {
     const square = squares[i];
 
-    console.log(x, square.x, square.width, square)
+    // console.log(x, square.x, square.width, square)
     if (x && x > square.x && x < square.x + square.width) {
-      // delete square, to replace with half-size A and B
-      squares.splice(i, 1);
-      splitOnX(square, x);
-      console.table(squares)
+      // randomly delete square, to replace with half-size A and B
+      if (Math.random() > 0.75) {
+        squares.splice(i, 1);
+        splitOnX(square, x);
+      }
+      // console.table(squares)
     }
     
     if (y && y > square.y && y < square.y + square.height) {
-      squares.splice(i, 1);
-      splitOnY(square, y);
-      console.table(squares)
+      if (Math.random() > 0.75) {
+        squares.splice(i, 1);
+        splitOnY(square, y);
+      }
+      // console.table(squares)
     }
   }
 }
 
 function splitOnX(square, splitAt) {
-  console.log('splittingX')
+  // countSX+=1;
+  // console.log('splittingX')
   const squareA = {
     x: square.x,
     y: square.y,
-    width: splitAt + square.x,
+    width: splitAt - square.x,
     height: square.height
   }
   const squareB = {
-    x: splitAt,
+    x: splitAt, 
     y: square.y,
-    width: square.width- splitAt,
+    width: square.x + square.width - splitAt,
     height: square.height
   }
 
@@ -58,25 +72,38 @@ function splitOnX(square, splitAt) {
 }
 
 function splitOnY(square, splitAt) {
+  // countSY += 1;
   console.log('splittingY')
   const squareA = {
     x: square.x,
     y: square.y,
     width: square.width,
-    height: splitAt + square.y
+    height: splitAt - square.y
   }
   const squareB = {
     x: square.x,
     y: splitAt,
     width: square.width,
-    height: square.height - splitAt
+    height: square.y + square.height - splitAt
   }
 
   squares.push(squareA);
   squares.push(squareB);
 }
 
+for (let i = 0; i <= size; i+= step) {
+  splitSquaresAt({ y: i });
+  splitSquaresAt({ x: i });
+}
+
+
 function draw() {
+  console.table(squares)
+  for (let i = 0; i < colors.length; i++) {
+    squares[Math.floor(Math.random() * squares.length)].color = colors[i];
+    console.log(squares)
+  }
+
   for (let i = 0; i < squares.length; i++) {
     ctx.beginPath();
     ctx.rect(
@@ -85,12 +112,17 @@ function draw() {
       squares[i].width,
       squares[i].height,
     );
+    if (squares[i].color) {
+      ctx.fillStyle = squares[i].color;
+    } else {
+      ctx.fillStyle = white;
+    }
+    ctx.fill();
     ctx.stroke();
   }
 }
 
-splitSquaresAt({ x: 200 });
-splitSquaresAt({ y: 250 })
+
 
 
 draw();
